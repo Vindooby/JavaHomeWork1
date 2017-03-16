095d89f1fc49657876c619a9515e38be792862ba
Vincent Wetzel <vincent.wetzel14@gmail.coms>
2017-03-16 17:21:40 -0400

    added pages

---
+++ main.js
@@ -0,0 +1,90 @@
+// Question 1//
+
+  var number = 15
+var number2 = 16
+
+console.log (number + number2)
+
+// Question 2//
+
+var text = "this is text "
+var text2 = "this is also text"
+
+console.log(text + text2);
+
+// Question 3//
+
+var beer = {
+  american:"bud",
+  english:"newcsatle",
+  german:"heineken",
+  dutch:"stella artois",
+    brews: [
+      "ipa",
+      "pilsner",
+      "light",
+      "draught"]
+}
+
+console.log(beer);
+
+var sports = [["hockey","baseball","golf","football"],["ice hockey","softball","flag football"]]
+
+console.log(sports[1][2]);
+
+var numb = 15
+  if(numb<100){
+    alert("This is less than 100")
+
+  } else {
+    alert("this is greaters than 100")
+  }
+
+// Question 4//
+
+    var numb = 150
+      if(numb<100){
+        alert("This is less than 100")
+
+      } else {
+        alert("this is greaters than 100")
+      }
+
+// Question 5//
+var phrase = prompt ("This is a phrase")
+
+  if
+  (phrase == "this is a phrase"){
+alert("thisi s right")
+}
+  else{
+    alert("you fkd up")
+  }
+
+
+// Question 5//
+var chooseName= prompt("Enter A Name")
+function someName(name){
+
+  return(name);
+}
+alert(someName(chooseName))
+
+// // Question 7//
+function vinnie(){
+  console.log("Hey")
+}
+vinnie();
+
+// QUestion 8//
+function doorPrize(numberofdoor){
+  if(doorPrize == 1){
+    alert("Congrats")
+  } else if (doorPrize == 2){
+    alert("you suck")
+  }  else if (doorPrize == 3){
+      alert("yay")
+  } else {
+      alert("try again no prize")
+    }
+  }
